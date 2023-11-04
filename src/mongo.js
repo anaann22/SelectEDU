4ae81992409f54925dcb157a0704661a7c3a5529
Ana Burlacu <114864127+anaann22@users.noreply.github.com>
2023-11-04 16:28:56 +0200

    adaugare mongo legatura

---
+++ src/mongo.js
@@ -0,0 +1,20 @@
+const {MongoClient} = require('mongodb');
+async function main() {
+	const uri = "mongodb+srv://ana:sasa@cluster0.mongodb.net/selectEDU?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    await client.connect();
+    await listDatabases(client);
+    try {
+        await client.connect();
+    
+        await listDatabases(client);
+     
+    } catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
