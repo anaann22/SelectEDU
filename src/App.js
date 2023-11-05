03cf142205e2318658b80002cd1a5c6c6aac2b39
Alexandra Cojocari <alexandra.cojocari02@e-uvt.ro>
2023-11-06 01:01:58 +0200

    login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import email from './Poze/email.png';
+import pass from './Poze/reset-password.png';
+import { Button } from 'react-native-web';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-component">
+        <a>Bine ai venit!</a>
+        <a>Aveti doar o posibilitate de a alege disciplina complementară. Aceasta nu poate fi modificată după, alegeți cu precauție și citiți fișa disciplinei.</a>
+        <a>După procesul de selecție și distribuire, veți primi un mail, cu confirmarea alegerii și orarul.</a>
+      </div>
+
+      <div className="right-component">
+        <a>Login</a>
+
+        <div class="input-container">
+          <img src={email} class="input-image" alt="Image Description"/>
+          <input type="text" class="input-field" id="Email" placeholder="Email"/>
+        </div>
+
+        <div class="input-container">
+          <img src={pass} class="input-image" alt="Image Description"/>
+          <input type="text" class="input-field" id= "Password" placeholder="Password"/>
+        </div>
+
+        <button className="button">Login</button>
+      </div>
     </div>
   );
 }
