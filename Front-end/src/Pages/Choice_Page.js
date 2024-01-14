417bb4a5154407e0a03ede7bcd83e232596d8154
anaann22 <ana.burlacu00@e-uvt.ro>
2024-01-14 17:44:37 +0200

    ana modif
    logout

--- Front-end/src/Pages/Choice_Page.js
+++ Front-end/src/Pages/Choice_Page.js
@@ -8,43 +8,54 @@ import confirm from '../Poze/confirm.png';
 import { useNavigate } from 'react-router-dom';
 
 const Choice_Page = () => {
-
     const navigate = useNavigate();
-
+  
     const navigateTo = (route) => {
       navigate(route);
     };
-
+  
+    const handleLogout = () => {
+        localStorage.removeItem('userToken'); 
+    
+        window.history.replaceState(null, '', '/login');
+        navigateTo("/");
+      };
+  
     return (
-        <div className='body'>
-            <div className="container">
-
-                {/* Card for "Citire documentatie" */}
-                <div className="middle-choice">
-                    <button onClick={() => navigateTo("/ReadDocumentation")}>Ghid utilizare</button>
-                    <img src={guide} className="img" alt="Middle Choice Image"/>
-                </div>
-
-                {/* Card for "Vizualizare discipline" */}
-                <div className="left-choice">
-                    <button onClick={() => navigateTo("/User")}>Vizualizare discipline complementare</button>
-                    <img src={book} className="img" alt="Left Choice Image"/>
-                </div>
-
-                {/* Card for "Alegere discipline" */}
-                <div className="right-choice">
-                    <button onClick={() => navigateTo("/UserChoice")}>Alegere discipline complementare</button>
-                    <img src={school} className="img" alt="Right Choice Image"/>
-                </div>
-
-                {/* Card for "Confirmare" */}
-                <div className="confirm-choice">
-                    <button onClick={() => navigateTo("/Confirm")}>Confirmare</button>
-                    <img src={confirm} className="img" alt="Confirm Choice Image"/>
-                </div>
-            </div>
-         </div>
+      <div className='body'>
+        <div className="container">
+          {/* Card for "Citire documentatie" */}
+          <div className="middle-choice">
+            <button onClick={() => navigateTo("/ReadDocumentation")}>Ghid utilizare</button>
+            <img src={guide} className="img" alt="Middle Choice Image"/>
+          </div>
+  
+          {/* Card for "Vizualizare discipline" */}
+          <div className="left-choice">
+            <button onClick={() => navigateTo("/User")}>Vizualizare discipline complementare</button>
+            <img src={book} className="img" alt="Left Choice Image"/>
+          </div>
+  
+          {/* Card for "Alegere discipline" */}
+          <div className="right-choice">
+            <button onClick={() => navigateTo("/UserChoice")}>Alegere discipline complementare</button>
+            <img src={school} className="img" alt="Right Choice Image"/>
+          </div>
+  
+          {/* Card for "Confirmare" */}
+          <div className="confirm-choice">
+            <button onClick={() => navigateTo("/Confirm")}>Confirmare</button>
+            <img src={confirm} className="img" alt="Confirm Choice Image"/>
+          </div>
+  
+          {/* Logout button */}
+          <div className="button-card2">
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        </div>
+      </div>
     );
-};
-
-export default Choice_Page;
+  };
+  
+  export default Choice_Page;
+  
